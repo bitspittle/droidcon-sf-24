f123ca5b3512623991a861c2b91ebbd2cb42b47a
David Herman <bitspittle@gmail.com>
2024-05-18 08:47:58 -0700

    Filter out sections from export

--- site/build.gradle.kts
+++ site/build.gradle.kts
@@ -23,6 +23,13 @@ kobweb {
                 link(rel = "stylesheet", href = routePrefix.prependTo("/reveal.js/dist/theme/white.css"))
             }
         }
+        export {
+            // Current, the Kobweb Markdown plugin creates one page per markdown file; but for this site, we only use
+            // them as slides under the index page. So just export the index page. (In a future version of Kobweb, the
+            // Markdown plugin will be updated to allow generating non-pages).
+            // See also: https://github.com/varabyte/kobweb/issues/301
+            filter.set { this.route == "/" }
+        }
 
         // Only legacy sites need this set. Sites built after 0.16.0 should default to DISALLOW.
         // See https://github.com/varabyte/kobweb#legacy-routes for more information.
