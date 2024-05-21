cced13722a3056b2b3ab28ff9e98883bfd20d66f
David Herman <bitspittle@gmail.com>
2024-05-21 10:59:47 -0700

    Update SectionLayout to read data parameters from frontmatter

--- site/src/jsMain/kotlin/dev/bitspittle/droidcon24/components/layouts/SectionLayout.kt
+++ site/src/jsMain/kotlin/dev/bitspittle/droidcon24/components/layouts/SectionLayout.kt
@@ -1,11 +1,26 @@
 package dev.bitspittle.droidcon24.components.layouts
 
 import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.core.rememberPageContext
+import com.varabyte.kobwebx.markdown.markdown
 import org.jetbrains.compose.web.dom.Section
 
 @Composable
 fun SectionLayout(content: @Composable () -> Unit) {
-    Section {
+    val ctx = rememberPageContext()
+    val md = ctx.markdown!!
+
+    // Data attributes are a way to request behaviors from Reveal.js
+    val dataAttrs = md.frontMatter.keys
+        .filter { it.startsWith("data-") }
+        .mapNotNull { dataKey ->
+            val dataValue = md.frontMatter[dataKey]?.single() ?: return@mapNotNull null
+            dataKey to dataValue
+        }
+
+    Section(
+        attrs = { dataAttrs.forEach { (key, value) -> attr(key, value) } }
+    ) {
         content()
     }
 }
