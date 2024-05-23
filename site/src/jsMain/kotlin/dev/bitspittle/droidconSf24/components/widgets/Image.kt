76ca39815027e33546f7c943e7b38473123c6ca7
David Herman <bitspittle@gmail.com>
2024-05-23 15:22:44 -0700

    Update image to use "images/" folder

--- site/src/jsMain/kotlin/dev/bitspittle/droidconSf24/components/widgets/Image.kt
+++ site/src/jsMain/kotlin/dev/bitspittle/droidconSf24/components/widgets/Image.kt
@@ -1,10 +1,21 @@
 package dev.bitspittle.droidconSf24.components.widgets
 
 import androidx.compose.runtime.Composable
-import com.varabyte.kobweb.navigation.RoutePrefix
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Colors
+import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
+import com.varabyte.kobweb.compose.ui.modifiers.borderRadius
+import com.varabyte.kobweb.compose.ui.modifiers.margin
+import com.varabyte.kobweb.compose.ui.modifiers.padding
+import org.jetbrains.compose.web.css.cssRem
+import org.jetbrains.compose.web.css.percent
+import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.Img
 
 @Composable
 fun Image(src: String) {
-    Img(RoutePrefix.prepend(src))
+    Box(Modifier.backgroundColor(Colors.LightGray).borderRadius(5.px).margin(leftRight = 10.percent).padding(1.cssRem)) {
+        Img("images/$src")
+    }
 }
