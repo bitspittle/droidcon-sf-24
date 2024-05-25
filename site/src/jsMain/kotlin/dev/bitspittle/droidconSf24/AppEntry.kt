7a79d7ce95f15ef778674aef7a9092924aff3d6d
David Herman <bitspittle@gmail.com>
2024-05-25 14:58:35 -0700

    Move top-level styles to appentry and not index

--- site/src/jsMain/kotlin/dev/bitspittle/droidconSf24/AppEntry.kt
+++ site/src/jsMain/kotlin/dev/bitspittle/droidconSf24/AppEntry.kt
@@ -1,9 +1,30 @@
 package dev.bitspittle.droidconSf24
 
 import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.css.UserSelect
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.userSelect
 import com.varabyte.kobweb.core.App
 import com.varabyte.kobweb.core.KobwebApp
+import com.varabyte.kobweb.silk.init.InitSilk
+import com.varabyte.kobweb.silk.init.InitSilkContext
+import com.varabyte.kobweb.silk.init.registerStyleBase
 import com.varabyte.kobweb.silk.prepareSilkFoundation
+import com.varabyte.kobweb.silk.style.layer.SilkLayer
+import com.varabyte.kobweb.silk.style.layer.add
+
+@InitSilk
+fun initSilk(ctx: InitSilkContext) {
+    ctx.stylesheet.apply {
+        // Layers specified in build.gradle.kts
+        cssLayers.add("reveal", "highlightjs", after = SilkLayer.BASE)
+
+        // Disable text selection as we don't need it for slides
+        registerStyleBase("body") {
+            Modifier.userSelect(UserSelect.None)
+        }
+    }
+}
 
 @App
 @Composable
--- site/src/jsMain/kotlin/dev/bitspittle/droidconSf24/pages/Index.kt
+++ site/src/jsMain/kotlin/dev/bitspittle/droidconSf24/pages/Index.kt
@@ -1,21 +1,14 @@
 package dev.bitspittle.droidconSf24.pages
 
 import androidx.compose.runtime.Composable
-import com.varabyte.kobweb.compose.css.UserSelect
 import com.varabyte.kobweb.compose.dom.disposableRef
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.classNames
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
-import com.varabyte.kobweb.compose.ui.modifiers.userSelect
 import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.Page
-import com.varabyte.kobweb.silk.init.InitSilk
-import com.varabyte.kobweb.silk.init.InitSilkContext
-import com.varabyte.kobweb.silk.init.registerStyleBase
-import com.varabyte.kobweb.silk.style.layer.SilkLayer
-import com.varabyte.kobweb.silk.style.layer.add
 import dev.bitspittle.droidconSf24.bindings.revealjs.Reveal
 import dev.bitspittle.droidconSf24.bindings.revealjs.RevealHighlight
 import dev.bitspittle.droidconSf24.bindings.revealjs.RevealNotes
@@ -25,19 +18,6 @@ import dev.bitspittle.droidconSf24.pages.sections.QandAPage
 import dev.bitspittle.droidconSf24.pages.sections.TitlePage
 import org.jetbrains.compose.web.dom.Div
 import kotlin.js.json
-
-@InitSilk
-fun initSilk(ctx: InitSilkContext) {
-    ctx.stylesheet.apply {
-        // Layers specified in build.gradle.kts
-        cssLayers.add("reveal", "highlightjs", after = SilkLayer.BASE)
-
-        // Disable text selection as we don't need it for slides
-        registerStyleBase("body") {
-            Modifier.userSelect(UserSelect.None)
-        }
-    }
-}
 
 @Page
 @Composable
