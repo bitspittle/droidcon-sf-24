34d7225723aaed795e5a5bb5342a957d8d37dd65
David Herman <bitspittle@gmail.com>
2024-05-29 10:58:49 -0700

    Add package mapping routing slide

--- site/src/jsMain/kotlin/dev/bitspittle/droidconSf24/components/widgets/Folders.kt
+++ site/src/jsMain/kotlin/dev/bitspittle/droidconSf24/components/widgets/Folders.kt
@@ -4,18 +4,20 @@ import androidx.compose.runtime.Composable
 import com.varabyte.kobweb.compose.css.ListStyleType
 import com.varabyte.kobweb.compose.css.TextAlign
 import com.varabyte.kobweb.compose.ui.Modifier
-import com.varabyte.kobweb.compose.ui.graphics.Colors
-import com.varabyte.kobweb.compose.ui.modifiers.color
 import com.varabyte.kobweb.compose.ui.modifiers.fontFamily
 import com.varabyte.kobweb.compose.ui.modifiers.listStyle
 import com.varabyte.kobweb.compose.ui.modifiers.textAlign
+import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.silk.style.CssStyle
-import com.varabyte.kobweb.silk.style.toAttrs
+import com.varabyte.kobweb.silk.style.toModifier
 import dev.bitspittle.droidconSf24.utilities.walk
 import kotlinx.browser.document
 import kotlinx.dom.addClass
 import org.jetbrains.compose.web.dom.Span
-import org.w3c.dom.*
+import org.w3c.dom.HTMLLIElement
+import org.w3c.dom.HTMLOListElement
+import org.w3c.dom.HTMLUListElement
+import org.w3c.dom.Text
 
 val FoldersStyle = CssStyle {
     base {
@@ -27,8 +29,8 @@ val FoldersStyle = CssStyle {
 }
 
 @Composable
-fun Folders(content: @Composable () -> Unit) {
-    Span(attrs = FoldersStyle.toAttrs {
+fun Folders(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
+    Span(attrs = FoldersStyle.toModifier().then(modifier).toAttrs {
         ref { element ->
             element.children.walk { child ->
                 when (child) {
---
+++ site/src/jsMain/resources/markdown/sections/kobweb/basics/Routing6.md
@@ -0,0 +1,26 @@
+---
+data-auto-animate:
+---
+
+## <span data-id="title">Route overrides</span>
+
+{{{ UrlBar("mysite.com/releases/1.0.0/screenshots", id = "url") }}}
+
+```kotlin 0|2|0 [code]
+// com/mysite/pages/releases/_1_0_0/PackageMapping.kt
+@file:PackageMapping("1.0.0")
+
+package com.mysite.pages.releases._1_0_0
+
+import com.varabyte.kobweb.core.PackageMapping
+```
+
+{{{ Folders(Modifier.classNames("fragment"))
+
+* pages
+  * releases 
+    * _1_0_0
+      * PackageMapping.kt
+      * Screenshots.kt
+
+}}}
